028076d0c9fefee8861c445c19f37db20c1ae42c
theisolatedmonk <96rahulseth@gmail.com>
2023-10-26 20:03:45 +0530

    mobile design implimented

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ImageBlock from '@/components/ImageBlock'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <Image src={'/images/mobile/image-hero.jpg'} alt={''} className='w-full ' width={100} height={100} />
       <div className="flex flex-col gap-40 z-10 p-4 text-white top-4 absolute w-full h-screen ">
         <div className="flex justify-between w-full ">
-          <Image src={'/images/logo.svg'} alt={'/images/logo.svg'} width={140} height={140} className='' />
+          <Link href={'/'}>   <Image src={'/images/logo.svg'} alt={'/images/logo.svg'} width={140} height={140} className='' /></Link>
           <Image src={'/images/icon-hamburger.svg'} alt={'/images/logo.svg'} width={30} height={30} className='' />
         </div>
         <div className='border-2 text-left top-64 text-5xl p-8 left-2 font-JosefinSans'>IMMERSIVE EXPERIENCES THAT DELIVER</div>
@@ -18,10 +19,34 @@ export default function Home() {
       <Image src={'/images/mobile/image-interactive.jpg'} alt={''} className='w-full p-4 mt-14 ' width={100} height={100} />
       <div className="flex flex-col p-12 text-center gap-4  font-JosefinSans"><div className='   text-4xl  '>The LEADER IN INTTERACTIVE VR </div>
         <p className="font-JosefinSans font-semibold text-darkGray text-lg">Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand. </p></div>
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full items-center gap-8">
         <div className='   text-4xl text-center font-JosefinSans  '>OUR CREATIONS</div>
         <ImageBlock />
-        <p className="border-2 p-2 px-4 text-center border-black">SEE ALL</p>
+        <p className="border-2 p-2 px-8 text-center border-black justify-center font-JosefinSans font-semibold">SEE ALL</p>
+      </div>
+      <div className="flex flex-col bg-black text-white items-center gap-4 p-10 mt-10 font-semibold">
+        <Image src={'/images/logo.svg'} alt={'/images/logo.svg'} width={140} height={140} className='' />
+        <ul className="flex gap-4 flex-col items-center justify-center ">
+          <tr>About</tr>
+          <tr>Carrers</tr>
+          <tr>Events</tr>
+          <tr>Products</tr>
+          <tr>Support</tr>
+        </ul>
+
+        <div className="flex  gap-4">
+          <Link href={'/'}>   <Image src={'/images/icon-facebook.svg'} alt={''} width={40} height={40} className='' /></Link>
+          <Link href={'/'}>        <Image src={'/images/icon-twitter.svg'} alt={''} width={40} height={40} className='' /></Link>
+          <Link href={'/'}><Image src={'/images/icon-pinterest.svg'} alt={''} width={40} height={40} className='' /></Link>
+          <Link href={'/'}> <Image src={'/images/icon-instagram.svg'} alt={''} width={40} height={40} className='' /></Link>
+
+          {/* <Image src={'/images/icon-facebook.svg'} alt={''} width={40} height={40} className='' /> */}
+          {/* <Image src={'/images/icon-twitter.svg'} alt={''} width={40} height={40} className='' /> */}
+          {/* <Image src={'/images/icon-pinterest.svg'} alt={''} width={40} height={40} className='' /> */}
+          {/* <Image src={'/images/icon-instagram.svg'} alt={''} width={40} height={40} className='' /> */}
+        </div>
+
+        <p className="text-darkGray">2021 Loopstudios. All right reseved.</p>
       </div>
     </div>
   )
--- components/ImageBlock.tsx
+++ components/ImageBlock.tsx
@@ -3,28 +3,75 @@ import Image from 'next/image'
 
 type Props = {}
 const mobileImgs = [
-    { src: '/images/mobile/image-deep-earth.jpg', alt: 'earth' },
-    { src: '/images/mobile/image-night-arcade.jpg', alt: 'arcade' },
-    { src: '/images/mobile/image-soccer-team.jpg', alt: 'soccer' },
-    { src: '/images/mobile/image-grid.jpg', alt: 'grid' },
-    { src: '/images/mobile/image-from-above.jpg', alt: 'above' },
-    { src: '/images/mobile/image-pocket-borealis.jpg', alt: 'borealis' },
-    { src: '/images/mobile/image-curiosity.jpg', alt: 'curiosity' },
-    { src: '/images/mobile/image-fisheye.jpg', alt: 'fisheye' },
+    { imgName: 'DEEP EARTH', src: '/images/mobile/image-deep-earth.jpg', alt: 'earth' },
+    { imgName: 'NIGHT ARCADE ', src: '/images/mobile/image-night-arcade.jpg', alt: 'arcade' },
+    { imgName: 'SOCCER TEAM VR', src: '/images/mobile/image-soccer-team.jpg', alt: 'soccer' },
+    { imgName: 'THE GRID ', src: '/images/mobile/image-grid.jpg', alt: 'grid' },
+    { imgName: 'FROM UP ABOVE VR', src: '/images/mobile/image-from-above.jpg', alt: 'above' },
+    { imgName: 'POCKET BOREALIS', src: '/images/mobile/image-pocket-borealis.jpg', alt: 'borealis' },
+    { imgName: 'THE CURIOSITY', src: '/images/mobile/image-curiosity.jpg', alt: 'curiosity' },
+    { imgName: 'MAKE IT FISHEYE', src: '/images/mobile/image-fisheye.jpg', alt: 'fisheye' },
 ]
 
 export default function ImageBlock({ }: Props) {
     return (
-        <div className="flex flex-col items-center gap-2 w-full">
+        <div className="flex flex-col items-center gap-2 w-full p-4">
             {mobileImgs.map((icon) => (
-                <Image key={icon.alt}
-                    src={icon.src}
-                    alt={icon.alt}
-                    width={100}
-                    height={100}
-                    className='w-full'
-                />
+                <div className="relative w-full">
+                    <Image key={icon.alt}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={100}
+                        height={100}
+                        className='w-full'
+                    />
+                    <p className="absolute text-white shadow-md font-JosefinSans z-10 text-[35px] p-10 top-0 w-60 -left-5">{icon.imgName}</p>
+                </div>
             ))}
+
         </div>
     )
 }
+
+
+// import React from 'react'
+// import Image from 'next/image'
+// import earth from '/images/mobile/image-deep-earth.jpg'
+// import arcade from '/images/mobile/image-night-arcade.jpg'
+// import soccer from '/images/mobile/image-soccer-team.jpg'
+// import grid from '/images/mobile/image-grid.jpg'
+// import above from '/images/mobile/image-from-above.jpg'
+// import borealis from '/images/mobile/image-pocket-borealis.jpg'
+// import curiosity from '/images/mobile/image-curiosity.jpg'
+// import fisheye from '/images/mobile/image-fisheye.jpg'
+// type Props = {}
+// const mobileImgs = [
+//     { imgName: 'DEEP EARTH', mobileSrc:  'earth' , alt: 'earth' },
+//     { imgName: 'NIGHT ARCADE ', mobileSrc: arcade, alt: 'arcade' },
+//     { imgName: 'SOCCER TEAM VR', mobileSrc: soccer, alt: 'soccer' },
+//     { imgName: 'THE GRID ', mobileSrc: grid, alt: 'grid' },
+//     { imgName: 'FROM UP ABOVE VR', mobileSrc: above, alt: 'above' },
+//     { imgName: 'POCKET BOREALIS', mobileSrc: borealis, alt: 'borealis' },
+//     { imgName: 'THE CURIOSITY', mobileSrc: curiosity, alt: 'curiosity' },
+//     { imgName: 'MAKE IT FISHEYE', mobileSrc: fisheye, alt: 'fisheye' },
+// ]
+
+// export default function ImageBlock({ }: Props) {
+//     return (
+//         <div className="flex flex-col items-center gap-2 w-full p-4">
+//             {mobileImgs.map((icon) => (
+//                 <div className="relative w-full">
+//                     <Image key={icon.alt}
+//                         src={icon.mobileSrc}
+//                         alt={icon.alt}
+//                         width={100}
+//                         height={100}
+//                         className='w-full'
+//                     />
+//                     <p className="absolute text-white shadow-md font-JosefinSans z-10 text-[35px] p-10 top-0 w-60 -left-5">{icon.imgName}</p>
+//                 </div>
+//             ))}
+
+//         </div>
+//     )
+// }
